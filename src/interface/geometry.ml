d302dfce08108633f4228449a42f87a1573cd193
chloeboireaudevier <cbd65@hotmail.fr>
2026-01-20 13:05:07 +0100

    impl geometry + changes in interface

--- src/interface/geometry.ml
+++ src/interface/geometry.ml
@@ -21,10 +21,10 @@ sig
 	val ( -- ) : t -> t -> t
 	
 	(* Produit scalaire de 2 vecteurs *)
-	val ( *. ) : t -> t -> float
+	val ( **. ) : t -> t -> float
 	
 	(* Produit d'un float par un vecteur *)
-	val ( ** ) : float -> t -> t
+	val ( *** ) : float -> t -> t
 	
 	(* Division d'un vecteur par un float *)
 	val ( // ) : t -> float -> t
@@ -50,4 +50,24 @@ sig
 	(* Vecteur orthogonal d'un vecteur, transfo (-y, x)
 	 * equivalent à rotate vec PI/2 *)
 	val ortho : t -> t
+end
+
+module VectorImpl : Vector with type t = float*float =
+struct
+	type t = float*float
+	let (++) (x1,y1) (x2,y2) = (x1+.x2, y1 +. y2)
+	let (--) (x1,y1) (x2,y2) = (x1-.x2, y1 -. y2)
+	let ( **. ) (x1,y1) (x2,y2) = x1*.x2 +.y1 *. y2
+	let ( *** ) k (x1,y1)  = (k *. x1, k *. y1)
+	let (//) (x1,y1) k = if k = 0. then failwith "Erreur : division par zero"
+	else (x1/.k,y1/.k)
+	(*Part du principe que length est en fait norm*)
+	let length (x1,y1) = ( x1**2. +. y1**2. )**(1./.2.)
+	let normalize v = let norm = length v in if norm = 0. then (0.,0.) else norm *** v 
+	let rotate (x1,y1) angle = (x1*.cos(angle)+.y1*.sin(angle),y1*.cos(angle)-.x1*.sin(angle)) (* TODO : check si ça marche avec des angles pas dans la range 0-2pi*)
+	let angle v = v **. (1.,0.) (* TODO check si bien en sens trigo*)
+	let project v axe= ((v**.axe)***axe)// ((length axe)**(1./.2.))
+	let symetric (x1,y1) (a1,a2) = let (xi,yi) = project (x1,y1) (a1,a2) in (2.*.xi -. x1, 2.*.yi +. y1)
+	let ortho v = rotate v (Float.pi/.2.)
+
 end
