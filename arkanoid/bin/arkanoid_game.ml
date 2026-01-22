71e2e10e9547f9cd6125ae427905f04038455d48
Louis Chouane <chouane.louis@gmail.com>
2026-01-22 17:46:54 +0100

    ajout squelette game arkanoid

--- arkanoid/bin/arkanoid_game.ml
+++ arkanoid/bin/arkanoid_game.ml
@@ -20,7 +20,32 @@ struct
   type entity = Ball | Racket | Brick of int | MapBorder of bool
   type scene = (int * int * (P.dyna, entity) P.pool * (P.stat, entity) P.pool)
 
-  let start () = Flux.unfold (fun () -> None) ()
+  let init_scene = let stat_pool = List.fold_left
+    (fun pool (body, obj) -> P.add pool body obj)
+    P.empty_stat_pool
+    [
+      ((Shape.Rect (10., 5.), (10., 10.), (0., 0.), (0., 0.), false), Brick 2),
+      ((Shape.Rect (10., 5.), (30., 10.), (0., 0.), (0., 0.), false), Brick 2),
+      ((Shape.Rect (10., 5.), (10., 18.), (0., 0.), (0., 0.), false), Brick 1),
+      ((Shape.Rect (10., 5.), (30., 18.), (0., 0.), (0., 0.), false), Brick 1),
+      ((Shape.Rect (90., 90.), (45., -45.), (0., 0.), (0., 0.), false),MapBorder false),
+      ((Shape.Rect (90., 90.), (-45., 45.), (0., 0.), (0., 0.), false),MapBorder false),
+      ((Shape.Rect (90., 90.), (135., 45.), (0., 0.), (0., 0.), false),MapBorder false),
+      ((Shape.Rect (90., 90.), (45., 135.), (0., 0.), (0., 0.), false),MapBorder true),
+    ]
+  in let dyna_pool = List.fold_left
+    (fun pool (body, obj) -> P.add pool body obj)
+    P.empty_dyna_pool
+    [
+      ((Shape.Rect (20., 2.), (45., 80.), (0., 0.), (0., 0.), false), Racket),
+      ((Shape.Circle 3., (45., 70.), (0., -10.), (0., 8.), true), Ball),
+    ]
+  in (5, 0, dyna_pool, stat_pool)
+
+
+  let update (pv, score, pool1, pool2) = (pv, score, pool1, pool2)
+
+  let start () = Flux.unfold (fun s -> let s' = update s in Some ((s', s'))) init
 
   let draw (_, _, pool1, pool2) f =
     let wrap = fun ((shape, pos, _, _, _) : _ P.body) obj -> f shape pos
